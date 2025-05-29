4eff17950c6d7a116e6ddf38d953ca835312be7a
Mauro Romito <mauro.romito@element.io>
2025-05-29 12:18:17 +0200

    untrusted setting to keep existing behaviour

--- MatrixSDK/Crypto/CryptoMachine/MXCryptoMachine.swift
+++ MatrixSDK/Crypto/CryptoMachine/MXCryptoMachine.swift
@@ -563,8 +563,8 @@ extension MXCryptoMachine: MXCryptoRoomEventDecrypting {
             // The app does not use strict shields by default, in the future this will become configurable
             // per room.
             strictShields: false,
-            // TODO: Not sure what to put here
-            decryptionSettings: .init(senderDeviceTrustRequirement: .crossSigned)
+            // Keep existing legacy behaviour
+            decryptionSettings: .init(senderDeviceTrustRequirement: .untrusted)
         )
     }
     
